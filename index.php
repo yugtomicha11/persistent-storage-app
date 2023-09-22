f4e64a8db8a7dcc87a0b49076f91b401cedcca57
DaGingaNinja87 <129398423+DaGingaNinja87@users.noreply.github.com>
2023-09-22 11:07:08 -0400

    Update index.php
    Added image link

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?>
+<?php
+echo '<p>Hello World</p>';
+$filepath= '/var/lib/www/images/world.jpeg'; 
+echo '<img src="'.$filepath.'">'; 
+?>
 </body>
 </html>
