d0c205d0307fcd8fa116ddba6eaab457a8da277f
David G <ncdavgreene@gmail.com>
2023-10-16 13:45:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
 <div class="image-grid">
     <?php
-    $dir = '/var/lib/data/galleries/';
+    $dir = 'galleries/';
     $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
 
     if (is_dir($dir)) {
